6f11532b02bfeb66e87cffc0a092771e3b1cce89
Andrew Hall <andrew.hall@uipath.com>
2019-06-27 16:02:22 -0700

    Adding source

---
+++ AzureResourceAutoManagement/AutoVmShutdown.cs
@@ -0,0 +1,78 @@
+using System;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace AzureResourceAutoManagement
+{
+    public static class AutoVmShutdown
+    {
+        [FunctionName(nameof(AutoVmShutdown))]
+        public static void Run([TimerTrigger("0 00 1 * * *"
+#if DEBUG 
+            ,RunOnStartup =true 
+#endif
+            )]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"Starting C# Timer trigger function at: {DateTime.Now}");
+
+            AzureCredentials credentials = GetAzureCredentials();
+
+            var azure = Azure
+            .Configure()
+            .Authenticate(credentials)
+            .WithDefaultSubscription();
+
+            var list = azure.VirtualMachines.List();
+            foreach (var vm in list)
+            {
+                var state = vm.PowerState;
+
+                if (ShouldShutdown(vm, log))
+                {
+                    log.LogInformation($"Powering off VM async");
+
+                    vm.PowerOffAsync();
+                }
+            }
+
+            log.LogInformation($"Completing C# Timer trigger function at: {DateTime.Now}");
+        }
+
+        private static AzureCredentials GetAzureCredentials()
+        {
+            string clientid = Environment.GetEnvironmentVariable("ClientId", EnvironmentVariableTarget.Process);
+            string clientSecret = Environment.GetEnvironmentVariable("ClientSecret", EnvironmentVariableTarget.Process);
+            string tenantId = Environment.GetEnvironmentVariable("TenantId", EnvironmentVariableTarget.Process);
+            
+            AzureCredentials credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientid, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
+
+            return credentials;
+        }
+
+        private static bool ShouldShutdown(IVirtualMachine vm, ILogger log)
+        {
+            if(vm.PowerState != PowerState.Running)
+            {
+                return false;
+            }
+
+            bool shouldShutdown = true;
+
+            string optoutList = Environment.GetEnvironmentVariable("OptedOutVms", EnvironmentVariableTarget.Process);
+            if (!string.IsNullOrEmpty(optoutList))
+            {
+                string[] optedoutVms = optedoutVms = optoutList.Split(';');
+                shouldShutdown = !optedoutVms.Contains(vm.Name);
+                log.LogInformation($"VM {vm.Name} opted out of auto-shutdown");
+            }
+
+            return shouldShutdown;
+        }
+    }
+}
